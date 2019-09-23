2f1c224630764b2c8803d329f7f5daab7556ca1f
Alexey Adamovich <alex.adamovich83@gmail.com>
2019-09-23 18:17:21 +0300

    Wait commit

--- dao/src/main/java/com/github/alexeysa83/finalproject/AuthUserDao.java
+++ dao/src/main/java/com/github/alexeysa83/finalproject/AuthUserDao.java
@@ -1,7 +1,7 @@
 package com.github.alexeysa83.finalproject;
 
 public interface AuthUserDao {
-
+// will wait until this comment is deleted
     void saveAuthUser (AuthUser user);
 
     AuthUser createAuthUser (String login, String password, boolean isAdmin);
