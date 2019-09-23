ac79165a193f7dc79bc9a6c8c1ee569be6cc6501
Alexey Adamovich <alex.adamovich83@gmail.com>
2019-09-23 18:07:11 +0300

    AuthUserDao crazy delete

--- dao/src/main/java/com/github/alexeysa83/finalproject/AuthUserDao.java
+++ dao/src/main/java/com/github/alexeysa83/finalproject/AuthUserDao.java
@@ -1,12 +1,3 @@
 package com.github.alexeysa83.finalproject;
 
-public interface AuthUserDao {
-
-    void saveAuthUser (AuthUser user);
-
-    AuthUser createAuthUser (String login, String password, boolean isAdmin);
-
-    AuthUser getAuthUserByLogin (String login);
-
-    boolean checkLoginIsTaken (String login);
 }
