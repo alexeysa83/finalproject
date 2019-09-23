5a4063dd405a34375ef71918958d54862b545897
Alexey Adamovich <alex.adamovich83@gmail.com>
2019-09-23 18:10:33 +0300

    Unhealthy proposal

--- dao/src/main/java/com/github/alexeysa83/finalproject/AuthUserDao.java
+++ dao/src/main/java/com/github/alexeysa83/finalproject/AuthUserDao.java
@@ -2,6 +2,8 @@ package com.github.alexeysa83.finalproject;
 
 public interface AuthUserDao {
 
+    // Think we can delete this class
+
     void saveAuthUser (AuthUser user);
 
     AuthUser createAuthUser (String login, String password, boolean isAdmin);
